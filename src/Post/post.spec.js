70fb1a305455fcdb4990d86cde9564fbb7969570
kolesnikaleksv <kolesnikaleksv@gmail.com>
2024-04-19 11:03:36 +0300

    Covered with tests  Post component

---
+++ src/Post/post.spec.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Post from "./post";
+import { shallow } from "enzyme";
+
+//use beforeEach
+
+const setUp = (props) => shallow(<Post {...props} />);
+
+describe("should redner Post component", () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  it("should contain .post wrapper", () => {
+    const wrapper = component.find(".post");
+    expect(wrapper.length).toBe(1);
+  })
+  
+  it("should contain link", () => {
+    const wrapper = component.find("a");
+    expect(wrapper.length).toBe(1);
+  })
+
+  it("should render created date", () => {
+    const created_at = "2_02_2024";
+    component = setUp({ created_at });
+    const date = component.find(".date");
+    expect(date.text()).toBe(new Date(created_at).toLocaleDateString());
+    console.log(created_at)
+  })
+})
+
+// after refactoring
+
+// const setUp = (props) => shallow(<Post {...props} />);
+
+// describe("should redner Post component", () => {
+//   it("should contain .post wrapper", () => {
+//     const component = setUp();
+//     const wrapper = component.find(".post");
+//     expect(wrapper.length).toBe(1);
+//   })
+  
+//   it("should contain link", () => {
+//     const component = setUp();
+//     const wrapper = component.find("a");
+//     expect(wrapper.length).toBe(1);
+//   })
+// })
+
+//Befor refactoring
+
+// describe("should redner Post component", () => {
+//   it("should contain .post wrapper", () => {
+//     const component = shallow(<Post/>);
+//     const wrapper = component.find(".post");
+//     expect(wrapper.length).toBe(1);
+//   })
+  
+//   it("should contain link", () => {
+//     const component = shallow(<Post/>);
+//     const wrapper = component.find("a");
+//     expect(wrapper.length).toBe(1);
+//   })
+// })
